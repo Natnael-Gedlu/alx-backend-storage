8a5af0a1858368aa8d88d618131c5c0d06ed9e4e
Natnael-Gedlu <nategd800@gmail.com>
2024-05-19 14:15:20 +0300

    Module for caching and counting URL accesses

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""
+Module for caching and counting URL accesses.
+"""
+
+import requests
+import redis
+from functools import wraps
+
+store = redis.Redis()
+
+
+def count_url_access(method):
+    """
+    Decorator to count and cache URL accesses.
+    """
+    @wraps(method)
+    def wrapper(url):
+        """
+        Wrapper to handle caching and counting.
+        """
+        cached_key = "cached:" + url
+        cached_data = store.get(cached_key)
+        if cached_data:
+            return cached_data.decode("utf-8")
+
+        count_key = "count:" + url
+        html = method(url)
+
+        store.incr(count_key)
+        store.set(cached_key, html)
+        store.expire(cached_key, 10)
+        return html
+    return wrapper
+
+
+@count_url_access
+def get_page(url: str) -> str:
+    """
+    Fetches a web page.
+    """
+    res = requests.get(url)
+    return res.text
